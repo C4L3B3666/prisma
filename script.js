12bded29a26aa5636b32e1a02d6100c6029cec21
C4L3B3666 <calebec055@gmail.com>
2025-11-01 13:22:19 +0100

    over

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.getElementById("menu_hamburguer").addEventListener("click", ()=> {
     document.querySelector("header").classList.toggle("ativo")
 
     if (document.querySelector("header").classList.contains("ativo"))
-        document.querySelector("body").style.overflow = "hidden"
+        document.querySelector("html").style.overflow = "hidden"
     else
-        document.querySelector("body").style.overflowY = "auto"
+        document.querySelector("html").style.overflowY = "auto"
 })
 
 
 document.querySelectorAll(".link_menu").forEach(link => {
     link.addEventListener("click", ()=> {
         document.querySelector("header").classList.remove("ativo")
-        document.querySelector("body").style.overflowY = "auto"
+        document.querySelector("html").style.overflowY = "auto"
     })
 })
